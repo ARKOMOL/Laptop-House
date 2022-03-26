261b44834466ba286e394cfa2002a17cbac6d8bb
ARKOMOL <80365662+ARKOMOL@users.noreply.github.com>
2022-03-26 22:47:12 +0600

    questions ans done

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ function App() {
       <h1>Laptop House</h1>
       <h3>Choose Your Laptop</h3>
       <Shop></Shop>
-      <div>
-        <h1>Question 1</h1>
+     <div className="Question-part">
+     <div className='question1'>
+        <h1>State Vs Props </h1>
         <h4>
           State:
           1.States are mutable.
@@ -24,6 +25,15 @@ function App() {
           3.We can pass from parent to child.And it mostly use to communicate between components.
         </h4>
       </div>
+      <div className="question2">
+    <h1>
+      How React Works
+    </h1>
+    <h4>
+      React is a popular library in javaScript which is developed by Facebook.It's can make single page application.Normally a website using DOM(Document Object Model) which is too slow for html and xml documents.That's why Racat use Virtual Dom.Virtual Dom most efficent way to update the browser's Dom.React Virtual DOM takes care of updating the DOM to match the React elements.And this is too fast.
+    </h4>
+      </div>
+     </div>
     </div>
   );
 }
